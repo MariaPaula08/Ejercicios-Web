615c0d05e14f730e9422f43c6286f1c65f430e05
MARIA <mariapaulaembuspolanco@gmail.com>
2023-04-17 06:09:02 -0500

    login

---
+++ Ejerciciosjs001/java.js
@@ -0,0 +1,47 @@
+/*Ejercicios Prácticos: 
+En estos ejercicios básicos realizaran las funciones y mostraran sus valores a través de la consola, Utilizaran ES6 y buenas prácticas en el código.*/
+
+//* 1. Crea una función que reciba dos números y devuelva el mayor de ellos. */
+
+const obtenerMayor=(numero1, numero2) => {
+    if(numero1 > numero2) {
+      return numero1
+    } else {
+      return numero2
+    }
+  }
+  console.log(obtenerMayor(8, 15))
+  
+  /*2. Crea una función que reciba un número y devuelva true si es par y false si es impar.*/
+  
+const numImpar=(num)=>{
+  if (condition) {
+    
+  }
+}
+  
+  
+  /*3. Crea una función que reciba un número y devuelva su valor absoluto.*/
+  /*4. Crea una función que reciba una cadena de texto y devuelva true si la cadena tiene más de 10 caracteres y false si no.*/
+  /*5. Crea una función que reciba dos cadenas de texto y devuelva true si ambas cadenas tienen la misma longitud y false si no.*/
+  /*6. Crea una función que reciba un número y devuelva true si está entre 20 y 50 (incluyendo ambos extremos) y false si no.*/
+  /*7. Crea una función que reciba un objeto con dos propiedades (name y age) y devuelva un mensaje de bienvenida que incluya el nombre y la edad.*/
+  /*8. Crea una función que reciba un array de números y devuelva la suma de todos ellos.*/
+  /*9. Crea una función que reciba un array de números y devuelva true si todos son positivos y false si no.*/
+  /*10. Crea una función que reciba un array de cadenas de texto y devuelva true si todas tienen más de 5 caracteres y false si no.*/
+  
+  /*Ejercicios:*/
+  /*1. Solicita al usuario su edad y muestra un mensaje que indique si es mayor de edad o no.*/
+  /*2. Pide al usuario que ingrese un número y muestra un mensaje que indique si es positivo o negativo.*/
+  /*3. Solicita al usuario su nombre y su edad y muestra un mensaje que indique si es mayor de edad o no.*/
+  /*4. Pide al usuario que ingrese un número y muestra un mensaje que indique si es par o impar.*/
+  /*5. Solicita al usuario su nombre y su género, y muestra un mensaje que indique si es hombre o mujer.*/
+  
+  /*Ejercicios
+  Realizaran estos 5 ejercicios utilizando bucles con ES6, tomando la información a través del prompt y mostrando los resultados en un alert:*/
+  /*1. Realizar una suma de los números ingresados por el usuario hasta que ingrese un número negativo.*/
+  /*2. Mostrar los números del 1 al 10 en orden ascendente y luego en orden descendente.*/
+  /*3. Pedir al usuario una cantidad de números y mostrar cuántos son pares y cuántos impares.*/
+  /*4. Realizar la tabla de multiplicar del número ingresado por el usuario.*/
+  /*5. Pedir al usuario un número y mostrar si es primo o no.*/
+  
